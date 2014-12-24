9ed7af67825231560042f5472f5ce750ffc4e9f4
BhagyashriPatil <bhagyashree060@gmail.com>
2014-12-24 16:08:45 +0530

    On item Click listener is added

--- GridViewApp/src/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gridviewapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,8 +25,12 @@ public class MainActivity extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View v, int position,
 					long id) {
-				
+				Intent i = new Intent(MainActivity.this,SingleViewActivity.class);
+				i.putExtra("id", position);
+				startActivity(i);
 			}
+
+			
 		});
     }
 
